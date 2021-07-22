c1813be86f60fa21ae7281602664becdd6e4afc7
dchaudhery <dea.chaudhery@gmail.com>
2021-07-22 19:10:06 -0400

    update chatbot.py

--- chatpbot.py
+++ chatpbot.py
@@ -1,26 +1,38 @@
 from chatterbot import ChatBot
+
+chatbot = ChatBot(
+    'CoronaBot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+
+# Training With Own Questions 
 from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
-
-# Creating ChatBot Instance
-chatbot = ChatBot('CoronaBot')
-
- # Training with Personal Ques & Ans 
-conversation = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
 
 trainer = ListTrainer(chatbot)
-trainer.train(conversation)
 
-# Training with English Corpus Data 
+training_data_quesans = open('training_data/answers.txt').read().splitlines()
+training_data_personal = open('training_data/sample_questions.txt').read().splitlines()
+
+training_data = training_data_quesans + training_data_personal
+
+trainer.train(training_data)
+
+# Training With Corpus
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
 trainer_corpus = ChatterBotCorpusTrainer(chatbot)
+
 trainer_corpus.train(
     'chatterbot.corpus.english'
-) 
+)
